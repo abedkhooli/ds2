4ed158fa4e9bf509daec27f5fa3e3562f6eb013c
Abed khooli <abedkhooli@users.noreply.github.com>
2018-12-02 19:31:59 +0200

    Print raw labels

--- ulmfit/train_clas.py
+++ ulmfit/train_clas.py
@@ -178,7 +178,8 @@ def get_datasets(dataset, dataset_dir, bptt, bs, lang, max_vocab, ds_pct, lm_typ
         print(f"Making the dataset smaller {ds_pct}")
     for split in [TRN, VAL, TST]:
         ids[split] = np.array([np.array(e, dtype=np.int) for e in ids[split]])
-        print([lbl for lbl in lbls[split] if not int(lbl) in [0,1,2]])          # debug by ak
+        #print([lbl for lbl in lbls[split] if not int(lbl) in [0,1,2]])          # debug by ak
+        print(f'First 10 lbls[split] labels: {lbls[split][:10]}') 
         lbls[split] = np.array([np.array(e, dtype=np.int) for e in lbls[split]]) # tried np.int(e) instead of e
     data_lm = TextLMDataBunch.from_ids(path=tmp_dir, vocab=vocab, train_ids=np.concatenate([ids[TRN],ids[TST]]),
                                        valid_ids=ids[VAL], bs=bs, bptt=bptt, lm_type=lm_type)
