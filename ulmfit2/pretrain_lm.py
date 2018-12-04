7f48587ffb8c3b6616233ec74a79bfb2d62e9a64
Abed khooli <abedkhooli@users.noreply.github.com>
2018-12-04 17:27:01 +0200

    changed drop mult and bs

--- ulmfit2/pretrain_lm.py
+++ ulmfit2/pretrain_lm.py
@@ -69,13 +69,13 @@ class LMHyperParams:
 
     # these hyperparameters are for training on ~100M tokens (e.g. WikiText-103)
     # for training on smaller datasets, more dropout is necessary
-    drop_mult = 0.1
+    drop_mult = 0.3  # was 0.1, ed by AK 
     dps = [0.25, 0.1, 0.2, 0.02, 0.15]
     clip: float = 0.12
     bptt: int = 70
-    bs: int = 70
+    bs: int = 32 # was 70 ed by AK
 
-    lang: str = 'en'
+    lang: str = 'ar'  # was 'en'
     name: str = None
     cuda_id: InitVar[int] = 0
 
