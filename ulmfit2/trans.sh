ad23b066b7cc1d711ecd065f913c6ee148a06752
Abed khooli <abedkhooli@users.noreply.github.com>
2018-12-15 08:12:15 +0200

    Update trans.sh

--- ulmfit2/trans.sh
+++ ulmfit2/trans.sh
@@ -3,9 +3,12 @@
 
 TRANS="py-googletrans"
 # Check if directory exists
-if [ ! -d "${TRANS}" ]; then
-  git clone https://github.com/ssut/py-googletrans.git
-  cd "${TRANS}"
-  python setup.py install
-  cd ..
-fi
+#if [ ! -d "${TRANS}" ]; then
+#  git clone https://github.com/ssut/py-googletrans.git
+#  cd "${TRANS}"
+#  python setup.py install
+#  cd ..
+#fi
+cd "${TRANS}"
+python setup.py install
+cd ..
