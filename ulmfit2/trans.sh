81033ad0dbec7d5ca872f77d86574ceafdbde45d
Abed khooli <abedkhooli@users.noreply.github.com>
2018-12-15 08:07:45 +0200

    Create trans.sh

---
+++ ulmfit2/trans.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+# Script to download a Wikipedia dump
+
+TRANS="py-googletrans"
+# Check if directory exists
+if [ ! -d "${TRANS}" ]; then
+  git clone https://github.com/ssut/py-googletrans.git
+  cd "${TRANS}"
+  python setup.py install
+  cd ..
+fi
